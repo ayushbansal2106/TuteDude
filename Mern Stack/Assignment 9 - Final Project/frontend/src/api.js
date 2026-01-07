c71c45f57f94851a6b02df72ba94fad216e6efc7
Ayush <pk3535745@gmail.com>
2026-01-08 00:19:09 +0530

    updated backend url

--- Mern Stack/Assignment 9 - Final Project/frontend/src/api.js
+++ Mern Stack/Assignment 9 - Final Project/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://visitor-backend-c5gm.onrender.com',
 });
 
 // This automatically adds the token to requests if we are logged in
